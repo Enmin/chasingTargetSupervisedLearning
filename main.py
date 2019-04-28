2b135090a51f3d513ebb7c153627ae9efa7f7dcb
Enmin Zhou <zhouem14@gmail.com>
2019-04-28 16:00:16 -0700

    fix main

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import numpy as np
 import itertools as it
+import tensorflow as tf
 import supervisedLearning as SL
 import neuralNetwork as NN
 import gridEnv as GE
 
 if __name__ == '__main__':
+	np.random.seed(128)
+	tf.set_random_seed(128)
 	gridSize = 10
 	actionSpace = [[0,1], [1,0], [-1,0], [0,-1], [1,1], [-1,-1], [1,-1], [-1,1]]
 	agentStates = [state for state in it.product([x for x in range(10)], [y for y in range(10)])]
@@ -15,7 +18,8 @@ if __name__ == '__main__':
 	maxTimeStep = int(gridSize * gridSize / 2)
 	learningRate = 0.01
 	dataNum = 2000
-	maxEpisode = 300
+	maxEpisode = 10000
+	summaryPeriod = 500
 	totalStateBatch = []
 	totalActionBatch = []
 	print('Generating Optimal Policy...')
@@ -24,7 +28,7 @@ if __name__ == '__main__':
 
 	transitionFunction = GE.TransitionFunction(gridSize)
 	generatePolicyNet = NN.GeneratePolicyNet(numOfState, numOfaction, learningRate, optimalPolicy, actionSpace)
-	model = generatePolicyNet(2, 128)
+	model = generatePolicyNet(2, 256)
 	graph = model.graph
 	isTerminal = GE.IsTerminal()
 	sampleTrajectory = SL.SampleTrajectory(maxTimeStep, transitionFunction, isTerminal, actionSpace, optimalPolicy)
@@ -32,16 +36,22 @@ if __name__ == '__main__':
 	for stateBatch, actionBatch in batch:
 		[totalStateBatch.append(np.array(state)) for state in stateBatch]
 		[totalActionBatch.append(np.array(action)) for action in actionBatch]
-	print(len(totalStateBatch))
-	print(len(totalActionBatch))
 	totalActionBatchLabel = [[1 if (action == actionSpace[index]).all() else 0 for index in range(len(actionSpace))] for action in totalActionBatch]
+	print("Training Data Per Episode: {}".format(len(totalStateBatch)))
+
+	state_ = graph.get_tensor_by_name('inputs/state_:0')
+	actionLabel_ = graph.get_tensor_by_name('inputs/actionLabel_:0')
+	loss_ = graph.get_tensor_by_name('outputs/loss_:0')
+	trainOpt_ = graph.get_collection_ref(tf.GraphKeys.TRAIN_OP)[0]
+	summaryOp = graph.get_collection_ref('mergedSummary')[0]
+	summaryWriter = graph.get_collection_ref('writer')[0]
 
 	for episode in range(maxEpisode):
-		state_ = graph.get_tensor_by_name('inputs/state_:0')
-		actionLabel_ = graph.get_tensor_by_name('inputs/actionLabel_:0')
-		loss_ = graph.get_tensor_by_name('outputs/loss_:0')
-		trainOpt_ = graph.get_operation_by_name('train/adamOpt_')
-		loss, trainOpt = model.run([loss_, trainOpt_], feed_dict={state_: totalStateBatch, actionLabel_: totalActionBatchLabel})
+		if (episode + 1) % summaryPeriod == 0:
+			loss, trainOpt, summary = model.run([loss_, trainOpt_, summaryOp], feed_dict={state_: totalStateBatch, actionLabel_: totalActionBatchLabel})
+			summaryWriter.add_summary(summary, episode)
+		else:
+			loss, trainOpt = model.run([loss_, trainOpt_], feed_dict={state_: totalStateBatch, actionLabel_: totalActionBatchLabel})
 		print(loss)
 
 	print("Testing Model...")
