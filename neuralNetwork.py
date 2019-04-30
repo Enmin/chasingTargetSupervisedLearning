480843cd80cf2155f0021d87a6d6dd6ea8d6a16f
Jianzhi <ljzprivate@yahoo.com>
2019-04-29 18:07:28 -0700

    Updated neuralNetwork, added supervisedLearning_v2

--- neuralNetwork.py
+++ neuralNetwork.py
@@ -2,26 +2,24 @@ import tensorflow as tf
 
 
 class GeneratePolicyNet:
-	def __init__(self, numStateSpace, numActionSpace, learningRate, optimalPolicy, actionSpace):
+	def __init__(self, numStateSpace, numActionSpace, learningRate):
 		self.numStateSpace = numStateSpace
 		self.numActionSpace = numActionSpace
 		self.learningRate = learningRate
-		self.optimalPolicy = optimalPolicy
-		self.actionSpace = actionSpace
 
 	def __call__(self, hiddenDepth, hiddenWidth):
 		print("Generating Policy Net with hidden layers: {} x {} = {}".format(hiddenDepth, hiddenWidth, hiddenDepth * hiddenWidth))
 		with tf.name_scope("inputs"):
-			state_ = tf.placeholder(tf.float32, [None, 4], name="state_")
-			actionLabel_ = tf.placeholder(tf.int32, [None, 8], name="actionLabel_")
-			# accumulatedRewards_ = tf.placeholder(tf.float32, [None, ], name="accumulatedRewards_")
+			state_ = tf.placeholder(tf.float32, [None, self.numStateSpace], name="state_")
+			actionLabel_ = tf.placeholder(tf.int32, [None, self.numActionSpace], name="actionLabel_")
+			tf.add_to_collection("inputs", state_)
+			tf.add_to_collection("inputs", actionLabel_)
 
 		with tf.name_scope("hidden"):
 			# initWeight = tf.random_uniform_initializer(-0.03, 0.03)
 			# initBias = tf.constant_initializer(0.001)
 			fc1 = tf.layers.Dense(units=hiddenWidth, activation=tf.nn.relu)
 			a1_ = fc1(state_)
-
 			w1_, b1_ = fc1.weights
 			tf.summary.histogram("w1", w1_)
 			tf.summary.histogram("b1", b1_)
@@ -32,7 +30,6 @@ class GeneratePolicyNet:
 				fc = tf.layers.Dense(units=hiddenWidth, activation=tf.nn.relu)
 				aNext_ = fc(a_)
 				a_ = aNext_
-
 				w_, b_ = fc.weights
 				tf.summary.histogram("w{}".format(i), w_)
 				tf.summary.histogram("b{}".format(i), b_)
@@ -40,7 +37,6 @@ class GeneratePolicyNet:
 
 			fcLast = tf.layers.Dense(units=self.numActionSpace, activation=None)
 			allActionActivation_ = fcLast(a_)
-
 			wLast_, bLast_ = fcLast.weights
 			tf.summary.histogram("wLast", wLast_)
 			tf.summary.histogram("bLast", bLast_)
@@ -48,38 +44,40 @@ class GeneratePolicyNet:
 
 		with tf.name_scope("outputs"):
 			actionDistribution_ = tf.nn.softmax(allActionActivation_, name='actionDistribution_')
+
 			cross_entropy = tf.nn.softmax_cross_entropy_with_logits_v2(logits=actionDistribution_, labels=actionLabel_, name='cross_entropy')
-			actionDistributionIndex = tf.argmax(actionDistribution_, axis=1)
-			actionLabelIndex = tf.argmax(actionLabel_, axis=1)
-			accuracy = tf.reduce_mean(tf.cast(tf.equal(actionDistributionIndex, actionLabelIndex), tf.float32))
 			loss_ = tf.reduce_mean(cross_entropy, name='loss_')
-			tf.summary.scalar("loss", loss_)
-			tf.summary.scalar("accuracy", accuracy)
+			tf.add_to_collection("loss", loss_)
+			lossSummary = tf.summary.scalar("loss", loss_)
+
+			actionIndices_ = tf.argmax(actionDistribution_, axis=1)
+			actionLabelIndices_ = tf.argmax(actionLabel_, axis=1)
+			accuracy_ = tf.reduce_mean(tf.cast(tf.equal(actionIndices_, actionLabelIndices_), tf.float32))
+			tf.add_to_collection("accuracy", accuracy_)
+			accuracySummary = tf.summary.scalar("accuracy", accuracy_)
 
 		with tf.name_scope("train"):
 			optimizer = tf.train.AdamOptimizer(self.learningRate, name='adamOpt_')
 			gradVarPairs_ = optimizer.compute_gradients(loss_)
 			trainOp = optimizer.apply_gradients(gradVarPairs_)
-
 			tf.add_to_collection(tf.GraphKeys.TRAIN_OP, trainOp)
 
 			gradients_ = [tf.reshape(grad, [1, -1]) for (grad, _) in gradVarPairs_]
 			gradTensor_ = tf.concat(gradients_, 1)
-			gradMax_ = tf.reduce_max(gradTensor_)
-			gradMin_ = tf.reduce_min(gradTensor_)
-
-			tf.add_to_collection("gradRange", gradMin_)
-			tf.add_to_collection("gradRange", gradMax_)
-
+			gradNorm_ = tf.norm(gradTensor_)
+			tf.add_to_collection("gradNorm", gradNorm_)
 			tf.summary.histogram("gradients", gradTensor_)
-			tf.summary.scalar('gradMax', gradMax_)
-			tf.summary.scalar('gradMin', gradMin_)
+			tf.summary.scalar('gradNorm', gradNorm_)
 
+		fullSummary = tf.summary.merge_all()
+		evalSummary = tf.summary.merge([lossSummary, accuracySummary])
+		tf.add_to_collection("summaryOps", fullSummary)
+		tf.add_to_collection("summaryOps", evalSummary)
 
-		mergedSummary = tf.summary.merge_all()
-		tf.add_to_collection("mergedSummary", mergedSummary)
-		writer = tf.summary.FileWriter("./tbdata", graph=tf.get_default_graph())
-		tf.add_to_collection("writer", writer)
+		trainWriter = tf.summary.FileWriter("./tbdata/train", graph=tf.get_default_graph())
+		testWriter = tf.summary.FileWriter("./tbdata/test", graph=tf.get_default_graph())
+		tf.add_to_collection("writers", trainWriter)
+		tf.add_to_collection("writers", testWriter)
 
 		model = tf.Session()
 		model.run(tf.global_variables_initializer())
