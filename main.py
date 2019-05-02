6eccdd1b3b4f963e31cbc785180ec42b9f0cb4e5
Jianzhi <ljzprivate@yahoo.com>
2019-05-01 17:00:33 -0700

    Updated main.py, use data repetitively

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import numpy as np
 import itertools as it
 import tensorflow as tf
 import math
+import random
 import supervisedLearning as SL
 import neuralNetwork as NN
 import gridEnv as GE
@@ -10,6 +11,7 @@ import visualize as VI
 
 
 if __name__ == '__main__':
+	random.seed(128)
 	np.random.seed(128)
 	tf.set_random_seed(128)
 
@@ -31,24 +33,23 @@ if __name__ == '__main__':
 	sampleTrajectory = PD.SampleTrajectory(maxTimeStep, transitionFunction, isTerminal, reset)
 
 	trajNum = 5000
-	dataSetPath = "data.pkl"
+	dataSetPath = "all_data.pkl"
 	#PD.generateData(sampleTrajectory, optimalPolicy, trajNum, dataSetPath, actionSpace)
 	dataSet = PD.loadData(dataSetPath)
+	random.shuffle(dataSet)
 
-	conditionNum = 11
-
-	learningRate = 0.001
-	generatePolicyNet = NN.GeneratePolicyNet(4, 8, learningRate)
-	models = [generatePolicyNet(3, 32) for _ in range(conditionNum)]
-
-	trainingDataSizes = [20*(2**i) for i in range(conditionNum)]
-	trainingDataList = [PD.sampleData(dataSet, size) for size in trainingDataSizes]
+	trainingDataSizes = list(range(1000, 9900, 1000))
+	trainingDataList = [([state for state, _ in dataSet[:size]], [label for _, label in dataSet[:size]]) for size in trainingDataSizes]
 	testDataSize = 5000
 	testData = PD.sampleData(dataSet, testDataSize)
 
+	learningRate = 0.001
+	generatePolicyNet = NN.GeneratePolicyNet(4, 8, learningRate)
+	models = [generatePolicyNet(3, 32) for _ in range(len(trainingDataSizes))]
+
 	maxStepNum = 50000
 	reportInterval = 500
-	lossChangeThreshold = 1e-8
+	lossChangeThreshold = 1e-6
 	lossHistorySize = 10
 	train = SL.Train(maxStepNum, learningRate, lossChangeThreshold, lossHistorySize, reportInterval,
 					 summaryOn=False, testData=None)
@@ -56,8 +57,8 @@ if __name__ == '__main__':
 	trainedModels = [train(model, data) for model, data in zip(models, trainingDataList)]
 	# evalResults = [(SL.evaluate(model, trainingData), SL.evaluate(model, testData)) for trainingData, model in zip(trainingDataList, trainedModels)]
 
-	evalTrain = {("Train", math.log2(len(trainingData[0]))): SL.evaluate(model, trainingData) for trainingData, model in zip(trainingDataList, trainedModels)}
-	evalTest = {("Test", math.log2(len(trainingData[0]))): SL.evaluate(model, testData) for trainingData, model in zip(trainingDataList, trainedModels)}
+	evalTrain = {("Train", len(trainingData[0])): SL.evaluate(model, trainingData) for trainingData, model in zip(trainingDataList, trainedModels)}
+	evalTest = {("Test", len(trainingData[0])): SL.evaluate(model, testData) for trainingData, model in zip(trainingDataList, trainedModels)}
 	evalTrain.update(evalTest)
 
 	VI.draw(evalTrain, ["mode", "training_set_size"], ["Loss", "Accuracy"])
