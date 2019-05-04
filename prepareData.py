ecd69cd3b193ea9df7b57e11d8a8fca9fa4b4578
Enmin Zhou <zhouem14@gmail.com>
2019-05-03 17:26:47 -0700

    add prepareAllData function

--- prepareData.py
+++ prepareData.py
@@ -69,3 +69,39 @@ def sampleData(data, batchSize):
 	batchInput = [x for x, _ in batch]
 	batchOutput = [y for _, y in batch]
 	return batchInput, batchOutput
+
+
+class SampleByStep():
+	def __init__(self, maxTimeStep, transitionFunction):
+		self.maxTimeStep = maxTimeStep
+		self.transitionFunction = transitionFunction
+
+	def __call__(self, policy, initialAgentState, targetPosition):
+		oldState, action = initialAgentState, [0, 0]
+		trajectory = []
+
+		for time in range(self.maxTimeStep):
+			action = policy[oldState + targetPosition]
+			newState = self.transitionFunction(oldState, action)
+			trajectory.append((list(oldState + targetPosition), action))
+			oldState = newState
+			break
+		return zip(*trajectory)
+
+
+def generateAllData(sampleByStep, policy, stateSpace, actionSpace):
+	path = "all_data.pkl"
+	totalStateBatch = []
+	totalActionBatch = []
+	for agent, target in stateSpace:
+		if (np.array(agent) == np.array(target)).all():
+			continue
+		states, actions = sampleByStep(policy, agent, target)
+		totalStateBatch = totalStateBatch + list(states)
+		oneHotAction = [
+			[1 if (np.array(action) == np.array(actionSpace[index])).all() else 0 for index in range(len(actionSpace))]
+			for action in actions]
+		totalActionBatch = totalActionBatch + oneHotAction
+	dataSet = list(zip(totalStateBatch, totalActionBatch))
+	saveFile = open(path, "wb")
+	pickle.dump(dataSet, saveFile)
